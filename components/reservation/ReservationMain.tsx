50f8978357c89b7f821560865266ba6288c2e77c
hyemin <hmjang28@gmail.com>
2022-02-14 21:35:56 +0900

    [Add] delete reservation

--- components/reservation/ReservationMain.tsx
+++ components/reservation/ReservationMain.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useSelector } from '../../store';
 import ReservationCard from './ReservationCard';
+import { isEmpty } from 'lodash';
 
 const Container = styled.div`
 	width: 100%;
@@ -22,9 +23,11 @@ const ReservationMain: React.FC = () => {
 	return (
 		<Container>
 			<h2>예약된 캠핑장</h2>
-			{reservations.map((v, idx) => (
-				<ReservationCard reservation={v} key={idx} />
-			))}
+			{isEmpty(reservations) && <div>예약 내역이 없습니다. </div>}
+			{!isEmpty(reservations) &&
+				reservations.map((v, idx) => (
+					<ReservationCard reservation={v} key={idx} />
+				))}
 		</Container>
 	);
 };
--- pages/api/reservations/room/[id].tsx
+++ pages/api/reservations/room/[id].tsx
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Data from '../../../../lib/data/index';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+	// 해당 캠핑장의 모든 예약 내역 조회
 	if (req.method === 'GET') {
 		const { id } = req.query;
 		try {
